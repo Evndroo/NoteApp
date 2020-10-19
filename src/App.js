858fdd3139895dbe779a6bb8d41b8f2f6fa23b33
evndroo <evandro.slipjr@gmail.com>
2020-10-19 17:11:42 -0300

    adicionando controle de estado e passagem de propriedades entre os componentes.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import ListaDeNotas from "./components/ListaDeNotas/";
 import "./assets/App.css"
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+
+  createNote(titulo, texto){
+    const novaNota = {
+      titulo,
+      texto      
+    };
+    
+    // atribui ao estado, todo o estado antigo e reatribui no atributo notas o que ele já possuia e adiciona a nova nota
+    this.setState({
+      ...this.state,
+      notas:[
+        ...this.state.notas,
+        novaNota
+      ]
+    })
+  }
+
   render() { 
     return ( 
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro createNote={this.createNote.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,18 +2,44 @@ import React, { Component } from "react"
 import "./estilo.css"
 
 class FormularioCadastro extends Component {
+    
+    constructor(props){
+        super(props);
+        this.titulo = "";
+        this.texto = ""
+    }
+    
+    _handleTitleChange(event){
+        event.stopPropagation();
+        this.titulo = event.target.value;
+    }
+
+    _handleTextChange(event){
+        event.stopPropagation();
+        this.texto = event.target.value;
+    }
+
+    _handleSubmit(event){
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.props.createNote(this.titulo, this.texto);
+    }
+
     render() {
         return (
-            <form className="form-cadastro">
+            <form className="form-cadastro" onSubmit={this._handleSubmit.bind(this)}>
                 <input 
                     type="text" 
                     placeholder="Título" 
                     className="form-cadastro_input"
+                    onChange={this._handleTitleChange.bind(this)}
                 />
                 <textarea 
                     rows={15}
                     className="form-cadastro_input" 
-                    placeholder="Escreva sua nota..." 
+                    placeholder="Escreva sua nota..."
+                    onChange={this._handleTextChange.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">Criar nota</button>
             </form>
